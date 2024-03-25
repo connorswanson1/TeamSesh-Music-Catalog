b0c4e1ef4f1dbfcc193180227fa9b76845682663
connorswanson1 <connorswanson13@gmail.com>
2024-03-25 16:40:24 -0600

    Fix scroll to for song list

--- teamsesh-music-catalog-app/src/components/NavBar.js
+++ teamsesh-music-catalog-app/src/components/NavBar.js
@@ -6,13 +6,27 @@ function scrollToComponent(componentId) {
     document.getElementById(componentId).scrollIntoView({ behavior: 'smooth' });
 }
 
+function scrollToSongsList() {
+    const songsListElement = document.getElementById('songsList');
+    if (songsListElement) {
+        const headerOffset = 55; // Adjust this value based on the height of your fixed header/navbar
+        const elementPosition = songsListElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    }
+}
+
 function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-title">TeamSESH Database</div>
             <div className="navbar-links">
                 <button onClick={() => scrollToComponent('homePage')}>Home</button>
-                <button onClick={() => scrollToComponent('songsList')}>Songs</button>
+                <button onClick={scrollToSongsList}>Songs</button>
                 <button onClick={() => scrollToComponent('aboutPage')}>About</button>
             </div>
             <div className="navbar-contact">
