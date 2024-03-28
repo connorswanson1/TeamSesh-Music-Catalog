b7223634c71fe76f97e46c3976ec2e4c83e81884
connorswanson1 <connorswanson13@gmail.com>
2024-03-28 17:30:20 -0600

    Major style update, added pictures, fixed about page content and style

--- teamsesh-music-catalog-app/src/components/HomePage.js
+++ teamsesh-music-catalog-app/src/components/HomePage.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../styles/Home.css';
 import { scrollToSongsList } from './NavBar';
-import Image from '../assets/bones_bottomtext.jpeg';
+import Image from '../assets/bones_smoke.jpeg';
 
 function HomePage() {
     return (
         <div className="home-container">
             <div className="home-text">
-                <h1>Welcome to the <span className="gothic-font">TeamSesh</span> Database</h1>
+                <h1> <span className="gothic-font">TeamSesh</span> Database</h1>
                 <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, produced, or been featured on.
                     Songs can be fitered by the artist, the producer, album, or feature. </p>
                 <p> Songs can also be sorted by release date or alphabetically by title. Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.
                 </p>
-                <button class="explore-btn" onClick={scrollToSongsList}>Explore Now</button>
+                <button class="explore-btn" onClick={scrollToSongsList}>Explore Now ↓</button>
             </div>
             <div className="home-image-container">
                 <img src={Image} alt="Bones" className="home-photo" />
