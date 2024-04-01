e746e6e004b9fb2b39721ba197f238919627fddf
connorswanson1 <connorswanson13@gmail.com>
2024-04-01 12:31:55 -0600

    Add back to song button to about

--- teamsesh-music-catalog-app/src/components/AboutPage.js
+++ teamsesh-music-catalog-app/src/components/AboutPage.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../styles/About.css';
 import Image from '../assets/bones_bottomtext.jpeg';
+import { scrollToSongsList } from './NavBar';
 
 function AboutPage() {
+    const aboutRef = useRef(null);
+    const [showButton, setShowButton] = useState(false); // State to show or hide the button
+
+    // Handle the scroll event
+    useEffect(() => {
+        const handleScroll = () => {
+            const aboutPosition = aboutRef.current.offsetTop;
+            const aboutHeight = aboutRef.current.offsetHeight;
+            const scrollPosition = window.pageYOffset;
+
+            // Check if the scroll position is within the About section boundaries
+            if (scrollPosition >= aboutPosition && scrollPosition < aboutPosition + aboutHeight) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <div className="about-container">
+        <div className="about-container" ref={aboutRef}>
+            {showButton && (
+                <button className={`back-to-songs ${showButton ? 'visible' : ''}`} onClick={scrollToSongsList}>
+                    Back To Songs ↑
+                </button>
+            )}
             <div className="about-section">
                 <h2>About</h2>
                 <p>This page was created by me, Connor Swanson. I am a software engineer, and my friends
