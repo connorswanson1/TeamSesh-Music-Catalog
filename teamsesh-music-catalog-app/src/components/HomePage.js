a0eb13a1b39bd3907a44529b5c92b3d6cc4d0a8b
connorswanson1 <connorswanson13@gmail.com>
2024-05-02 17:46:36 -0600

    Style changes

--- teamsesh-music-catalog-app/src/components/HomePage.js
+++ teamsesh-music-catalog-app/src/components/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
     return (
         <div className="home-container">
             <div className="home-text">
-                <h1> <span className="gothic-font">TeamSesh</span> Database</h1>
+                <h1> <span className="gothic-font">TeamSesh Database</span></h1>
                 <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, produced, or been featured on.
                     Songs can be fitered by the artist, the producer, album, or feature. </p>
                 <p> Songs can also be sorted by release date or alphabetically by title. Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.
