0e5917192e7905ff5d3f316efe3e24f242f306f3
connorswanson1 <connorswanson13@gmail.com>
2024-05-07 12:38:48 -0600

    Additon of song search and home overhaul

--- teamsesh-music-catalog-app/src/components/HomePage.js
+++ teamsesh-music-catalog-app/src/components/HomePage.js
@@ -2,21 +2,23 @@ import React from 'react';
 import '../styles/Home.css';
 import { scrollToSongsList } from './NavBar';
 // import Image from '../assets/bones_smoke.jpeg';
+import SongSearch from './SongSearch';
 
 function HomePage() {
     return (
         <div className="home-container">
             <div className="home-text">
                 <h1> <span className="gothic-font">TeamSesh Database</span></h1>
-                <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, produced, or been featured on.
-                    Songs can be fitered by the artist, the producer, album, or feature. </p>
-                <p> Songs can also be sorted by release date or alphabetically by title. Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.
+                <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, produced, or been featured on. </p>
+                <p>To the right is a function for searching for a specific song, and below is the full database.</p>
+                <p> Songs can be fitered by the artist, the producer, album, or feature.
+                    Songs can also be sorted by release date or alphabetically by title. Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.
                 </p>
-                <button class="explore-btn" onClick={scrollToSongsList}>Explore Now ↓</button>
+                <button class="explore-btn" onClick={scrollToSongsList}>Explore Full Database ↓</button>
             </div>
-            {/* <div className="home-image-container">
-                <img src={Image} alt="Bones" className="home-photo" />
-            </div> */}
+            <div className="home-search-container">
+                < SongSearch />
+            </div>
         </div>
     );
 }
---
+++ teamsesh-music-catalog-app/src/components/SongSearch.js
@@ -0,0 +1,92 @@
+// Lots of redundancy in this file, need to create parent component to pass songs
+import React, { useState, useMemo, useEffect } from 'react';
+import axios from 'axios';
+import '../styles/LoadingIndicator.css';
+import '../styles/SongSearch.css';
+
+const SongSearch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedSong, setSelectedSong] = useState(null);
+
+    const [songs, setSongs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchSongs = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/api/songs/details');
+                setSongs(response.data);
+                if (response.data.length > 0) {
+                    // Select a random song
+                    const randomSong = response.data[Math.floor(Math.random() * response.data.length)];
+                    setSelectedSong(randomSong);
+                }
+                setIsLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setIsLoading(false);
+            }
+        };
+
+        fetchSongs();
+    }, []);
+
+    const filteredSongs = useMemo(() => {
+        if (searchTerm.length > 1) {
+            return songs.filter(song =>
+                song.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        }
+        return [];
+    }, [searchTerm, songs]);
+
+    const handleSelectSong = (song) => {
+        setSelectedSong(song);
+    };
+
+    const LoadingIndicator = () => {
+        return (
+            <div className="loading-container">
+                <span className="loading-text"><span className="gothic-font">Loading Song Search...</span></span>
+            </div>
+        );
+    };
+
+    if (isLoading) return <LoadingIndicator />;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div className="search-container">
+            <input
+                type="text"
+                className="search-input"
+                placeholder="Search for a song..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <ul className={`search-results ${filteredSongs.length > 0 ? 'expanded' : ''}`}>
+                {filteredSongs.map(song => (
+                    <li key={song.id} onClick={() => handleSelectSong(song)}>
+                        {song.title}
+                    </li>
+                ))}
+            </ul>
+            {selectedSong && (
+                <div className="song-details">
+                    <h3>{selectedSong.title}</h3>
+                    <p>Artist: {selectedSong.artist}</p>
+                    <p>Producer: {selectedSong.producer || 'N/A'}</p>
+                    <p>Album: {selectedSong.album || 'N/A'}</p>
+                    <p>Feature: {selectedSong.feature || 'N/A'}</p>
+                    <p>Release Date: {selectedSong.release_date ? new Date(selectedSong.release_date).toLocaleDateString() : 'Unknown'}</p>
+                    <div>{selectedSong.song_art_url ? <img src={selectedSong.song_art_url} alt="song art" /> : 'N/A'}</div>
+                </div>
+            )}
+        </div>
+    );
+
+
+};
+
+export default SongSearch;
