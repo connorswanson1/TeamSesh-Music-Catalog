d3d852eb63def0b630d1594dbffb7f270b95c80b
connorswanson1 <connorswanson13@gmail.com>
2024-05-17 18:49:22 -0600

    Connect db.js to server, require env in db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+// backend/server.js
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const axios = require('axios');
+const connectDB = require('./config/db'); // Import the connectDB function
 require('dotenv').config(); // To use environment variables from .env file
 
 // Initialize the app
@@ -13,10 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-const dbURI = process.env.MONGO_URI;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
+connectDB();
 
 // Define your routes here
 app.use('/api/artists', require('./routes/artist.routes')); // Linking artist routes
@@ -25,7 +22,7 @@ app.use('/api/songs', require('./routes/song.routes')); // Linking song routes
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../teamsesh-music-catalog-app/build')));
 
-// Anything that doesn't match the above, send back the index.html file
+// Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../teamsesh-music-catalog-app/build/index.html'));
 });
