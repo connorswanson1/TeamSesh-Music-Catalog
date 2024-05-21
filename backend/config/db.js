ac0d599015ff74f3bd27e2e6739b8969f192991f
connorswanson1 <connorswanson13@gmail.com>
2024-05-20 20:13:37 -0600

    Update db to use quotaguard

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,30 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+const url = require('url');
 
 const dbURI = process.env.MONGO_URI; // Make sure this is defined in your .env file
+const proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
 
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // ... any other options you might need
+    auth: {
+        user: process.env.MONGO_USER,
+        password: process.env.MONGO_PASSWORD,
+    },
+    proxy: {
+        protocol: proxy.protocol,
+        hostname: proxy.hostname,
+        port: proxy.port,
+        auth: proxy.auth,
+    },
 };
 
 const connectDB = async () => {
     try {
         await mongoose.connect(dbURI, options);
-        console.log("Successfully connected to MongoDB.");
+        console.log('Successfully connected to MongoDB.');
     } catch (error) {
-        console.error("MongoDB connection error:", error);
+        console.error('MongoDB connection error:', error);
         process.exit(1); // Exit process with failure
     }
 };
