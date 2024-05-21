59cfee69b786f9a7af86cf5d9965988ec9eb18eb
connorswanson1 <connorswanson13@gmail.com>
2024-05-21 11:46:03 -0600

    Remove proxy from db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
-const url = require('url');
 
 const dbURI = process.env.MONGO_URI; // Make sure this is defined in your .env file
-const proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
 
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    proxy: {
-        proxyUsername: new URL(proxyUrl).username,
-        proxyPassword: new URL(proxyUrl).password,
-        proxyHostname: new URL(proxyUrl).hostname,
-        proxyPort: new URL(proxyUrl).port
-    }
+    // Remove the unsupported proxy option
 };
 
 const connectDB = async () => {
