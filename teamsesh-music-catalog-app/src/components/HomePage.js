5f5d554b7cb5eb44d756502c6bdfa0d3456e47d6
connorswanson1 <connorswanson13@gmail.com>
2024-06-24 13:01:36 -0600

    Add hideable details on home page with styles

--- teamsesh-music-catalog-app/src/components/HomePage.js
+++ teamsesh-music-catalog-app/src/components/HomePage.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Home.css';
 import { scrollToSongsList } from './NavBar';
-// import Image from '../assets/bones_smoke.jpeg';
 import SongSearch from './SongSearch';
 
 function HomePage() {
+    const [isListVisible, setIsListVisible] = useState(false);
+
+    const toggleListVisibility = () => {
+        setIsListVisible(!isListVisible);
+    };
+
     return (
         <div className="home-container">
             <div className="home-text">
-                <h1> <span className="gothic-font">TeamSesh Database</span></h1>
-                <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, been featured on, or is associated with. </p>
-                <ul className="home-list">
+                <h1><span className="gothic-font">TeamSesh Database</span></h1>
+                <p>This page is a collection of every song <span className="gothic-font">BONES</span> has made, been featured on, or is associated with.</p>
+                <button className="toggle-list-btn" onClick={toggleListVisibility}>
+                    How can I use this site?
+                </button>
+                <ul className={`home-list ${isListVisible ? 'visible' : ''}`}>
                     <li>Songs can be filtered by the artist, the producer, album, or feature.</li>
                     <li>Songs are sorted by most recent by default, which can be flipped.</li>
                     <li>Clicking the title of a song will take you to the Genius lyrics page.</li>
                     <li>Search for a specific song, or access the full database below.</li>
                 </ul>
-                <p>Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.</p>
-                <button class="explore-btn" onClick={scrollToSongsList}>Explore Full Database ↓</button>
+                {/* <p>Thank you for visiting, enjoy. <span className="gothic-font">sesh</span>.</p> */}
+                <button className="explore-btn" onClick={scrollToSongsList}>Explore Full Database ↓</button>
             </div>
             <div className="home-search-container">
-                < SongSearch />
+                <SongSearch />
             </div>
         </div>
     );
